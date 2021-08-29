eb6b4d5881619c783daafe86ff3e8dd4e9d3c7ff
Steven Mu <xsmu0922@gmail.com>
2021-08-28 21:02:55 -0400

    deleted old splash files

--- splash/check.js
+++
@@ -1,9 +0,0 @@
-
-function getInfo(){
-    var roomCode = document.getElementById("roomCode").value
-
-    /* check through mainJSON here */
-    
-    /* If good, create new, if not, return string "no room with such code" */
-}
-
